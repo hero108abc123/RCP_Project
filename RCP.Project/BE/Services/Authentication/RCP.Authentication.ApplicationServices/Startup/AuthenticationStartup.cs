f236d7c9c5fd4a50a25183bef28eed00bff8d6b1
Giang Trinh <62133153+hero108abc123@users.noreply.github.com>
2025-09-14 20:54:00 +0700

    First Commit

---
+++ RCP.Project/BE/Services/Authentication/RCP.Authentication.ApplicationServices/Startup/AuthenticationStartup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RCP.Authentication.Infrastructure;
+using RCP.Shared.ApplicationService.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP.Authentication.ApplicationService.Startup
+{
+    public static class AuthenticationStartup
+    {
+        public static void ConfigureAuth(this WebApplicationBuilder builder, string? assemblyName)
+        {
+            builder.Services.AddDbContext<AuthenticationDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(
+                        builder.Configuration.GetConnectionString("Default"),
+                        options =>
+                        {
+                            options.MigrationsAssembly(assemblyName);
+                            options.MigrationsHistoryTable(
+                                DbSchema.TableMigrationsHistory,
+                                DbSchema.Authenticaiton
+                            );
+                        }
+                    );
+                },
+                ServiceLifetime.Scoped
+            );
+        }
+    }
+}
---
+++ RCP.Project/BE/Services/Authentication/RCP.Authentication.Infrastructure/AuthenticationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP.Authentication.Infrastructure
+{
+    public class AuthenticationDbContext : DbContext
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

0657fbd2174dd63d331aae143ed9f04220ef50f4
Giang Trinh <62133153+hero108abc123@users.noreply.github.com>
2025-10-08 10:28:19 +0700

    Permission Service

---
+++ RCP.Project/BE/Services/Authentication/RCP.Authentication.Dtos/Permission/ViewPermissionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP.Authentication.Dtos.Permission
+{
+    public class ViewPermissionDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; } = String.Empty;
+        public string Key { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+    }
+}

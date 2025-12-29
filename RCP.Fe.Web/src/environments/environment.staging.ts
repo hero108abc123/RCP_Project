6605d89f0119de0a9f465b5e9a011e0396d4a080
Kiin <vietcuong23122k2@gmail.com>
2025-12-29 13:07:02 +0700

    chore: deploy

--- RCP.Fe.Web/src/environments/environment.staging.ts
+++ RCP.Fe.Web/src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from "@/shared/models/environment.models";
 
 export const environment: IEnvironment = {
     production: false,
-    baseUrl: 'http://10.20.0.108:8100/api',
+    baseUrl: 'http://10.20.0.108:8100',
     authGrantType: 'password',
     authClientId: 'client-web',
     authClientSecret: 'mBSQUHmZ4be5bQYfhwS7hjJZ2zFOCU2e',

6d847cc55ef53b32c3152351521207b3f168d4c0
Kiin <vietcuong23122k2@gmail.com>
2025-12-29 15:23:22 +0700

    chore: update env

--- RCP.Fe.Web/src/environments/environment.staging.ts
+++ RCP.Fe.Web/src/environments/environment.staging.ts
@@ -1,11 +1,11 @@
 import { IEnvironment } from "@/shared/models/environment.models";
 
 export const environment: IEnvironment = {
-    production: false,
-    baseUrl: '',
+    production: true,
+    baseUrl: 'http://10.20.0.108:8100',
     authGrantType: 'password',
     authClientId: 'client-web',
     authClientSecret: 'mBSQUHmZ4be5bQYfhwS7hjJZ2zFOCU2e',
     authScope: 'openid offline_access',
-    appUrl: '',
+    appUrl: 'http://10.20.0.108:4200',
 };

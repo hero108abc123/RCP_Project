1436051e30d4b3e46a67eb1ee47634a319c4723b
Giang Trinh <62133153+hero108abc123@users.noreply.github.com>
2026-01-07 12:30:40 +0700

    fix giao diện

--- RCP.Fe.App/app/(screen)/movie-booking.tsx
+++ RCP.Fe.App/app/(screen)/movie-booking.tsx
@@ -1,10 +1,16 @@
-import { useLocalSearchParams, useRouter } from 'expo-router';
-import React from 'react';
-import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { useLocalSearchParams, useRouter } from "expo-router";
+import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Appbar } from "react-native-paper";
 
-import CinemaList from '@/app/(screen)/cinema/cinema-list';
-import DateSelector from '@/components/bookingComponents/date-selector';
+import CinemaList from "@/app/(screen)/cinema/cinema-list";
+import DateSelector from "@/components/bookingComponents/date-selector";
 
 type MovieParams = {
   id: string;
@@ -17,7 +23,7 @@ type MovieParams = {
 export default function MovieBooking() {
   const router = useRouter();
   const params = useLocalSearchParams();
-  
+
   // ✅ Lấy cả movie object và movieId
   const movie = params.movie ? JSON.parse(params.movie as string) : null;
   const movieId = params.movieId as string;
@@ -25,11 +31,16 @@ export default function MovieBooking() {
   if (!movie || !movieId) {
     return (
       <View style={{ flex: 1 }}>
-        <Appbar.Header style={{ backgroundColor: '#0B4A8B' }}>
-          <Appbar.BackAction onPress={() => router.back()} color='white'/>
-          <Appbar.Content title="ĐẶT VÉ THEO PHIM" titleStyle={{ color: '#fff', fontWeight: '700' }} />
+        <Appbar.Header style={{ backgroundColor: "#0B4A8B" }}>
+          <Appbar.BackAction onPress={() => router.back()} color="white" />
+          <Appbar.Content
+            title="ĐẶT VÉ THEO PHIM"
+            titleStyle={{ color: "#fff", fontWeight: "700" }}
+          />
         </Appbar.Header>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
           <Text>Không tìm thấy thông tin phim</Text>
         </View>
       </View>
@@ -39,9 +50,12 @@ export default function MovieBooking() {
   return (
     <View style={{ flex: 1 }}>
       {/* ---------- Header ---------- */}
-      <Appbar.Header style={{ backgroundColor: '#0B4A8B' }}>
-        <Appbar.BackAction onPress={() => router.back()} color='white'/>
-        <Appbar.Content title="ĐẶT VÉ THEO PHIM" titleStyle={{ color: '#fff', fontWeight: '700' }} />
+      <Appbar.Header style={{ backgroundColor: "#0B4A8B" }}>
+        <Appbar.BackAction onPress={() => router.back()} color="white" />
+        <Appbar.Content
+          title="ĐẶT VÉ THEO PHIM"
+          titleStyle={{ color: "#fff", fontWeight: "700" }}
+        />
       </Appbar.Header>
 
       {/* ---------- Banner ---------- */}
@@ -57,7 +71,7 @@ export default function MovieBooking() {
           style={styles.detailBtn}
           onPress={() =>
             router.push({
-              pathname: '/(screen)/movie-detail',
+              pathname: "/(screen)/movie-detail",
               params: {
                 movieId: movieId, // ✅ TRUYỀN movieId để fetch API
               },
@@ -67,10 +81,12 @@ export default function MovieBooking() {
           <Text style={styles.detailText}>Chi tiết phim</Text>
         </TouchableOpacity>
       </ImageBackground>
-      
+
       <View>
-        <DateSelector/>
-        <Text style={{marginLeft: 10, fontSize: 16, fontWeight: '800'}}>Chọn rạp xem</Text>
+        <DateSelector />
+        <Text style={{ marginLeft: 10, fontSize: 16, fontWeight: "800" }}>
+          Chọn rạp xem
+        </Text>
         <CinemaList />
       </View>
     </View>
@@ -80,28 +96,28 @@ export default function MovieBooking() {
 const styles = StyleSheet.create({
   banner: {
     height: 220,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     padding: 16,
   },
   movieTitle: {
     fontSize: 20,
-    fontWeight: '700',
-    color: '#fff',
+    fontWeight: "700",
+    color: "#000000",
   },
   movieInfo: {
-    color: '#E0E0E0',
+    color: "#E0E0E0",
     marginVertical: 6,
   },
   detailBtn: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingVertical: 6,
     paddingHorizontal: 14,
     borderRadius: 20,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginTop: 8,
   },
   detailText: {
-    color: '#0B4A8B',
-    fontWeight: '600',
+    color: "#0B4A8B",
+    fontWeight: "600",
   },
 });
